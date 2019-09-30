6091acc998b83423e1bce5af39c298dfc42bd453
enrparalv <enriquezpalvarez@gmail.com>
2019-09-30 15:05:40 +0200

    Fixed readData.py errors

--- Python/functions/readData.py
+++ Python/functions/readData.py
@@ -7,23 +7,31 @@
 # --------------------------------------------------------------------------
 
 def readData(fileName):
-	size = 0
+    size = 0
     observationData = []
     predictionData = []
-    data = 1
+    data = 0
 
     for line in open(fileName):
+        production=0
+        wind=0
+        
+        if line == "predicciones\n":
+        	data += 1
 
-        if line == "predicciones":
-        	data = 0
+        elif line != "observaciones" and data == 0:
+            spLine = line.split(" ")
+            
+            if len(spLine) == 4:
+                production = int(spLine[2])
+                wind = spLine[3].rstrip()
 
-    	elif line != "observaciones" and data == 1:
-            observationValue = [line.split(" ")[2], line.split(" ")[3]] 
+            observationValue = [production, wind] 
             observationData.append(observationValue)
             size+=1
 
-    	else:
-    		predictionValue = line.split(" ")[2] 
+        else:
+            predictionValue = line.split(" ")[2].rstrip() 
             predictionData.append(predictionValue)
-    
+            
     return observationData, predictionData, size
